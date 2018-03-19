9c8b8f5454e3b083887f81c47773bf7eda6c224b
YanLiang <yanliang1102@gmail.com>
2018-03-19 09:47:42 -0500

    Update findBestRealSolution.py

--- findBestRealSolution.py
+++ findBestRealSolution.py
@@ -17,17 +17,17 @@ solutioncount=0;
 globalbest=179769313486231590770839156793787453197860296048756011706444423684197180216158519368947833795864925541502180565485980503646440548199239100050792877003355816639229553136239076508735759914822574862575007425302077447712589550957937778424442426617334727629299387668709205606050270810842907692932019128194467627007;
 solutionDictionary={}
 def findTheRealSmallestPrime(rootdir):
-	#looping through all the files under the dir
-	for subdir, dirs, files in os.walk(rootdir):
-    		for file in files:
-        		 currentfile=os.path.join(subdir,file)
-        		 with open(currentfile	, 'rb') as inputdata:
-                        data=pickle.load(inputputdata,pickle.HIGHEST_PROTOCOL)
-                        first=data.pop();
-                        if(first.is_prime()):
-                        	globalbest=Math.min(globalbest,first);
-                        	solutioncount=solutioncount+1;
-                        	solutionDictionary[str(first)]=file;
+    #looping through all the files under the dir
+    for subdir, dirs, files in os.walk(rootdir):
+            for file in files:
+                 currentfile=os.path.join(subdir,file)
+                 with open(currentfile  , 'rb') as inputdata:
+                    data=pickle.load(inputputdata,pickle.HIGHEST_PROTOCOL)
+                    first=data.pop()
+                    if first.is_prime():
+                        globalbest=Math.min(globalbest,first);
+                        solutioncount=solutioncount+1;
+                        solutionDictionary[str(first)]=file;                          
     #store the solution dictionary and the best solution find on disk.
     solutionDictionary["solutioncount"]=solutioncount;
     solutionDictionary["globalbest"]=globalbest;
