ca8be5e1d633bfb1b7899fedac46b36aa292368a
YanLiang <yanliang1102@gmail.com>
2018-03-19 12:25:26 -0500

    Create getBestResult.py

---
+++ getBestResult.py
@@ -0,0 +1,21 @@
+
+import pickle
+class Stack():
+    def __init__(self):
+        self.__storage = []
+    def isEmpty(self):
+        return len(self.__storage) == 0
+    def push(self,p):
+        self.__storage.append(p)
+    def pop(self):
+        return self.__storage.pop()
+def exploreTheBestResult(bestfilename):
+    data=pickle.load(open(bestfilename,"rb"))
+    print(data["globalbest"]);
+    print(data["solutioncount"]);
+
+def main():
+    bestfilename=sys.argv[1]
+    exploreTheBestResult(bestfilename);
+if __name__ == "__main__":
+    main()
