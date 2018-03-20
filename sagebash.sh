70f1d0c4609f47ff914c573aa9bf8ea6a7ca2ee5
YanLiang <yanliang1102@gmail.com>
2018-03-19 20:31:00 -0500

    Update sagebash.sh

--- sagebash.sh
+++ sagebash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sage batchsage.py 42 1 "/home/yan/crypfinal/batch/"&
-sage batchsage.py 42 2 "/home/yan/crypfinal/batch/"&
-sage batchsage.py 42 3 "/home/yan/crypfinal/batch/"&
-sage batchsage.py 42 4 "/home/yan/crypfinal/batch/"&
-sage batchsage.py 42 5 "/home/yan/crypfinal/batch/"&
-sage batchsage.py 42 6 "/home/yan/crypfinal/batch/"&
-sage batchsage.py 42 7 "/home/yan/crypfinal/batch/"&
-sage batchsage.py 42 8 "/home/yan/crypfinal/batch/"&
-sage batchsage.py 42 0 "/home/yan/crypfinal/batch/"&
+sage batchsage.py 42 1 "/home/yan/crypfinal/batchnew1/"&
+sage batchsage.py 42 2 "/home/yan/crypfinal/batchnew1/"&
+sage batchsage.py 42 3 "/home/yan/crypfinal/batchnew1/"&
+sage batchsage.py 42 4 "/home/yan/crypfinal/batchnew1/"&
+sage batchsage.py 42 5 "/home/yan/crypfinal/batchnew1/"&
+sage batchsage.py 42 6 "/home/yan/crypfinal/batchnew1/"&
+sage batchsage.py 42 7 "/home/yan/crypfinal/batchnew1/"&
+sage batchsage.py 42 8 "/home/yan/crypfinal/batchnew1/"&
+sage batchsage.py 42 0 "/home/yan/crypfinal/batchnew1/"&
