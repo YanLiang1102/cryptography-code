27011b0a154626d02d5459bdf2d68de49498b15a
YanLiang <yanliang1102@gmail.com>
2018-03-20 01:07:30 -0500

    Create headbash.sh

---
+++ headbash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sage headfinal.py 6 8 1 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 2 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 3 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 4 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 5 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 0 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 6 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 7 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 8 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 8 9 "/home/yan/crypfinal/batchfinal/"&
+
+
