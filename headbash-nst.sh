ef3438aa91689d8fd4dfbf442b047c1747af7585
YanLiang <yanliang1102@gmail.com>
2018-03-21 20:48:04 -0500

    quick solution for no storage

---
+++ headbash-nst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sage headfinal-nst.py 3 15 1 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 2 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 3 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 4 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 5 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 0 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 6 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 7 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 8 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 15 9 "/home/yan/crypfinal/batchnst/"&
+
+
