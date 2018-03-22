396c0a74b38f1f3b7d2739a9d0511d90856adfe5
YanLiang <yanliang1102@gmail.com>
2018-03-21 21:03:22 -0500

    Update headbash-nst.sh

--- headbash-nst.sh
+++ headbash-nst.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sage headfinal-nst.py 3 15 1 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 2 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 3 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 4 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 5 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 0 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 6 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 7 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 8 "/home/yan/crypfinal/batchnst/"&
-sage headfinal-nst.py 3 15 9 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 1 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 2 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 3 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 4 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 5 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 0 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 6 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 7 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 8 "/home/yan/crypfinal/batchnst/"&
+sage headfinal-nst.py 3 8 9 "/home/yan/crypfinal/batchnst/"&
 
 
