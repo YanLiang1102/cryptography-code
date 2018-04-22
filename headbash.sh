35357a809ddd941e6d8554417646b88f209ae5e9
yan <yan@portland.cs.ou.edu>
2018-04-21 21:22:21 -0500

    make 32 thread to work on this

--- headbash.sh
+++ headbash.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sage headfinal.py 6 8 1 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 2 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 3 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 4 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 5 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 0 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 6 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 7 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 8 "/home/yan/crypfinal/batchfinal/"&
-sage headfinal.py 6 8 9 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 0 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 1 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 2 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 3 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 4 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 5 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 6 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 7 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 8 "/home/yan/crypfinal/batchfinal/"&
+sage headfinal.py 6 12 9 "/home/yan/crypfinal/batchfinal/"&
 
 
---
+++ lattice-attach/solution.py
@@ -0,0 +1,171 @@
+import cPickle as pickle
+import sys
+import os
+from sage.all import *
+import math
+
+def getMlist(mlist):
+    #with open('/Users/yanliang/cryptography/madata.data', 'a') as the_file:
+    p=2;
+    count=0;
+    while(count<100):
+        #data=math.floor((p)**(1/3)*(10**100));
+        data=math.floor(((p).n(prec=1000).nth_root(3))*(10**100))
+        #the_file.write(str(data)+'\n');
+        mlist.append(data);
+        count=count+1;
+        p=next_prime(p)
+    return mlist;
+     
+        
+def main():
+    index=sys.argv[1]; #will be 10^6
+    intindex=int(index);
+    #new code
+    mlist=[]
+    getMlist(mlist);
+    ar=[0,
+ 1,
+ 0,
+ 0,
+ 1,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1,
+ 1,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1,
+ 0,
+ 1,
+ 0,
+ 0,
+ 1,
+ 1,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1,
+ 1,
+ 0,
+ 1,
+ 0,
+ 1,
+ 0,
+ 1,
+ 1,
+ 1,
+ 1,
+ 1,
+ 0,
+ 1,
+ 0,
+ 1,
+ 1,
+ 1,
+ 0,
+ 1,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1,
+ 0,
+ 0,
+ 1,
+ 1,
+ 1,
+ 0,
+ 0,
+ 1,
+ 1,
+ 0,
+ 1,
+ 1,
+ 1,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 0,
+ 1];
+ fixed=[2,
+ 4,
+ 7,
+ 13,
+ 14,
+ 16,
+ 17,
+ 19,
+ 20,
+ 23,
+ 24,
+ 25,
+ 30,
+ 31,
+ 32,
+ 34,
+ 37,
+ 38,
+ 39,
+ 40,
+ 41,
+ 47,
+ 49,
+ 50,
+ 54,
+ 56,
+ 57,
+ 59,
+ 60,
+ 64];
+ #seperate into 2^5=0,1,2,...31 thread to do so each thread will handle 2^25
+sumInMemory=[]
+start=(2**25)*intindex
+for i in range(start,(2**25)+start):
+    t=intToB(i)
+    data=0;
+    for i in range(len(t)-1,-1,-1):
+        data=data+((int)(t[i]))*mlist[fixed[i]];
+    sumInMemory.append(data);
+with open('/home/yan/cryptography-code/fixed'+str(intindex)+".data", 'wb') as handle:
+    pickle.dump(sumInMemory, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    main()
